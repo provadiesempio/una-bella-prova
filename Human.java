5ba57ae042149c1998334ef0c15e054ac4c6bb87
provadiesempio <provadiesempio@mailinator.com>
2015-07-10 17:40:53 +0200

    Commit test(prova).
    Non so bene questo a cosa serva. Si vedrà.

---
+++ Human.java
@@ -0,0 +1,32 @@
+/**
+* Un bel commento iniziale!
+*
+*@author: Silvio Berlusconi
+*
+**/
+
+public class Human{
+
+//Properties
+private String name;
+private float height;
+private int age;
+
+//Constructor:
+public Human(String name;float height; int age){
+  this.age = age;
+  this.height = height;
+  this.name = name;
+}
+
+//Methods
+getName(){
+  return this.name;
+}
+
+setName(String name){
+  this.name = name;
+}
+
+  
+}
